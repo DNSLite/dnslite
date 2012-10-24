b61ea0bbe53c6255b7c3e5ec1d2d484bd5b0690e
Dejian Xu <xudejian@moode.cn>
2012-10-24 09:09:52 +0800

    将search add按钮放在下边

--- src/me/xu/DNSLite/HostsEditorActivity.java
+++ src/me/xu/DNSLite/HostsEditorActivity.java
@@ -98,8 +98,11 @@ public class HostsEditorActivity extends ListActivity {
 		setListAdapter(adapter);
 	}
 
-    public void btn_search_add_onclick(View v) {
+    public void btn_search_add_host_onclick(View v) {
         addHosts(0, filterText.getText().toString(), null);
+    }
+
+    public void btn_search_add_dns_onclick(View v) {
     }
 
     public void empty_view_onclick(View v) {
