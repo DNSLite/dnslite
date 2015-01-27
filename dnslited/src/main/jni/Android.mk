4ce003c152e7a23b111dfc85b09db37996effdea
Dejian Xu <xudejian2008@gmail.com>
2015-01-27 16:07:52 +0800

    add pic for 4.4+

--- dnslited/src/main/jni/Android.mk
+++ dnslited/src/main/jni/Android.mk
@@ -5,7 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := dnsproxy
 LOCAL_SRC_FILES := dnsproxy.cc net.c cache.cc dns.c nameserver.c android_sys_prop.c
 LOCAL_C_INCLUDES := .
-#LOCAL_CFLAGS := -std=gnu++0x
+LOCAL_CFLAGS += -pie -fPIE
+LOCAL_LDFLAGS += -pie -fPIE
 LOCAL_CPP_EXTENSION := .cc
 #LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
 
